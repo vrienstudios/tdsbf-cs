fc20086f907319d2fb526d190bf0dc7d238facd7
Chay <visionistprojects@gmail.com>
2020-08-08 14:50:53 -0400

    Update Channel.cs

--- Data/Discord/MessageClasses/Channel.cs
+++ Data/Discord/MessageClasses/Channel.cs
@@ -59,7 +59,7 @@ namespace TDSBF.Data.Discord.MessageClasses
         /// <returns></returns>
         public List<MessageClasses.Message> GetPastMessagesList(int amount)
         {
-            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.GetAsync(string.Format("https://discordapp.com/api/v6/channels/{0}/messages?limit={1}", this.id, amount.ToString()));
+            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.GetAsync(string.Format("https://discord.com/api/v6/channels/{0}/messages?limit={1}", this.id, amount.ToString()));
             if (!response.Result.IsSuccessStatusCode)
                 throw new Exception();
             System.Threading.Tasks.Task<string> str = response.Result.Content.ReadAsStringAsync();
@@ -74,10 +74,49 @@ namespace TDSBF.Data.Discord.MessageClasses
         public Boolean PostMessage(String content, Boolean? tts)
         {
             StringContent data = new StringContent(JsonConvert.SerializeObject(new Message(content, tts)), UnicodeEncoding.UTF8, "application/json");
-            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.PostAsync(string.Format("https://discordapp.com/api/v6/channels/{0}/messages", this.id), data);
+            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.PostAsync(string.Format("https://discord.com/api/v6/channels/{0}/messages", this.id), data);
             if (!response.Result.IsSuccessStatusCode)
-                return false;
+                Console.WriteLine(response.Result.StatusCode + " " + response.Result.ReasonPhrase + " " + string.Format("https://discord.com/api/v6/channels/{0}/messages", this.id) + " " + Storage.client.DefaultRequestHeaders.Authorization.ToString());
             return true;
+        }
+        /// <summary>
+        /// Post message to channel
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static Boolean PostMessage(String channelID, String content, Boolean? tts)
+        {
+            StringContent data = new StringContent(JsonConvert.SerializeObject(new Message(content, tts)), UnicodeEncoding.UTF8, "application/json");
+            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.PostAsync(string.Format("https://discord.com/api/v6/channels/{0}/messages", channelID), data);
+            if (!response.Result.IsSuccessStatusCode)
+                Console.WriteLine(response.Result.StatusCode + " " + response.Result.ReasonPhrase + " " + string.Format("https://discord.com/api/v6/channels/{0}/messages", channelID) + " " + Storage.client.DefaultRequestHeaders.Authorization.ToString());
+            return true;
+        }
+        /// <summary>
+        /// Post message to channel
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static MessageClasses.Message PostMessage2(String channelID, String content, Boolean? tts)
+        {
+            StringContent data = new StringContent(JsonConvert.SerializeObject(new Message(content, tts)), UnicodeEncoding.UTF8, "application/json");
+            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.PostAsync(string.Format("https://discord.com/api/v6/channels/{0}/messages", channelID), data);
+            if (!response.Result.IsSuccessStatusCode)
+                Console.WriteLine(response.Result.StatusCode + " " + response.Result.ReasonPhrase + " " + string.Format("https://discord.com/api/v6/channels/{0}/messages", channelID) + " " + Storage.client.DefaultRequestHeaders.Authorization.ToString());
+            return JsonConvert.DeserializeObject<MessageClasses.Message>(response.Result.Content.ReadAsStringAsync().Result);
+        }
+        /// <summary>
+        /// Post message to channel
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public MessageClasses.Message PostMessage2(String content, Boolean? tts)
+        {
+            StringContent data = new StringContent(JsonConvert.SerializeObject(new Message(content, tts)), UnicodeEncoding.UTF8, "application/json");
+            System.Threading.Tasks.Task<HttpResponseMessage> response = Storage.client.PostAsync(string.Format("https://discord.com/api/v6/channels/{0}/messages", this.id), data);
+            if (!response.Result.IsSuccessStatusCode)
+                Console.WriteLine(response.Result.StatusCode + " " + response.Result.ReasonPhrase + " " + string.Format("https://discord.com/api/v6/channels/{0}/messages", this.id) + " " + Storage.client.DefaultRequestHeaders.Authorization.ToString());
+            return JsonConvert.DeserializeObject<MessageClasses.Message>(response.Result.Content.ReadAsStringAsync().Result);
         }
     }
 }
